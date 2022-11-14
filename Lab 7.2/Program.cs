3e800a7920c8f9b03255e9ac265b0e2f66e92e6d
krist <krist@LAPTOP-HIQLHLL6>
2022-11-14 08:42:58 +0500

    Добавьте файлы проекта.

---
+++ Lab 7.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7._2
+{
+    class Program
+    {
+        static void GetOS(int a, out double v, out double s)
+        {
+         v = a * a * a;
+            s = a * a * 6;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double V;
+            double S;
+            GetOS(x, out V, out S);
+            Console.WriteLine("V={0:.00} , S={1:.00}",V,S);
+            Console.ReadKey();
+
+        }
+    }
+}
